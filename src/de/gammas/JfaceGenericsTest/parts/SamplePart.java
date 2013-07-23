865501ed4bb6c2e934984823bee8d03659e964e5
Hendrik Still <hendrik.still@gammas.de>
2013-07-23 19:20:49 +0200

    Using fully typed content provider

--- src/de/gammas/JfaceGenericsTest/parts/SamplePart.java
+++ src/de/gammas/JfaceGenericsTest/parts/SamplePart.java
@@ -1,4 +1,3 @@
-
 package de.gammas.JfaceGenericsTest.parts;
 
 import java.util.ArrayList;
@@ -19,43 +18,45 @@ import de.gammas.JfaceGenericsTest.model.Person;
 
 public class SamplePart {
 
-
 	@PostConstruct
 	public void createComposite(Composite parent) {
 		parent.setLayout(new GridLayout());
-		ComboViewer<Person> comboViewer = new ComboViewer<Person>(parent);
-		List<Person> content = new ArrayList<Person>();		
-		
+		ComboViewer<Person, List<Person>> comboViewer = new ComboViewer<Person, List<Person>>(
+				parent);
+		List<Person> content = new ArrayList<Person>();
+
 		content.add(new Person("Peter", new Date()));
 		content.add(new Person("Hans", new Date()));
-		
-		comboViewer.setContentProvider(new IStructuredContentProvider<Person>() {
-			private Person[] elements;
-			
-			public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-				
-			}
-			
-			public void dispose() {
-				
-			}
-			
-			public Person[] getElements(Object inputElement) {
-				
-				List<Person> personList =  (List<Person>) inputElement;
-				return personList.toArray(new Person[personList.size()]);
-			}
-		});
-		
-		comboViewer.setLabelProvider(new LabelProvider<Person>(){
+
+		comboViewer
+				.setContentProvider(new IStructuredContentProvider<Person, List<Person>>() {
+
+					public void inputChanged(Viewer<List<Person>> viewer,
+							List<Person> oldInput, List<Person> newInput) {
+						// TODO Auto-generated method stub
+
+					}
+
+					public void dispose() {
+
+					}
+
+					public Person[] getElements(List<Person> inputElement) {
+
+						List<Person> personList = inputElement;
+						return personList.toArray(new Person[personList.size()]);
+					}
+
+				});
+
+		comboViewer.setLabelProvider(new LabelProvider<Person>() {
 			@Override
 			public String getText(Person element) {
 				return element.getName();
 			}
 		});
 		comboViewer.setInput(content);
- 		
-		
+
 	}
 
 	@Focus
